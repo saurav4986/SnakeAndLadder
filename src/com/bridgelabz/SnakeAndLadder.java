45beac34c83c526107f67565b406f128021f9ec1
saurav4986 <saurav.raj4986@gmail.com>
2022-10-19 18:30:01 +0530

    Added: code where player rolls a die to get a no. 1-6

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -3,6 +3,20 @@ package com.bridgelabz;
 public class SnakeAndLadder {
     public static void main(String[] args) {
 
-        System.out.println("!!!Welcome to Snake & Ladder!!!");
+        int dice = (int)(Math.random() * 6);
+
+        if(dice ==1) {
+            System.out.println("The output is One");
+        }else if(dice ==2) {
+            System.out.println("The output is Two");
+        }else if(dice ==3) {
+            System.out.println("The output is Three");
+        }else if(dice ==4) {
+            System.out.println("The output is Four");
+        }else if(dice ==5) {
+            System.out.println("The output is Five");
+        }else {
+            System.out.println("The output is Six");
+        }
     }
 }
